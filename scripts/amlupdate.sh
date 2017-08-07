d14091dbf417fda65cdf2ff3f65c92ab2417b5c5
Ritwik Bhattacharya <ritwik.bhattacharya@gmail.com>
2017-08-07 11:53:53 -0700

    Removing spark batch installation from setup script.

--- scripts/amlupdate.sh
+++ scripts/amlupdate.sh
@@ -2,60 +2,12 @@
 
 #########################################################################################################
 #
-# System setup
-#
-# 3. AML SparkBatch
-# 4. AML CLI
+# CLI setup
 #
 #########################################################################################################
 
-## Create installation directory for AML bits (SparkBatch, Livy)
-AZUREML_INSTALLDIR=/opt/microsoft/azureml
-mkdir -p ${AZUREML_INSTALLDIR}
-
-## Create a temp directory for downloads, etc.
-echo "Setting up tmp directory"
-AZUREML_TMP=/tmp/azureml/
-rm -rf ${AZUREML_TMP}
-mkdir -p ${AZUREML_TMP}
-cd ${AZUREML_TMP}
-
-## Install SparkBatch
-echo "Installing azureml spark-batch"
-rm -rf ${AZUREML_INSTALLDIR}/azureml
-AZUREML_TARFILE=azureml.tar.gz
-AZUREML_TARFILEURI=https://amlsamples.blob.core.windows.net/apps/${AZUREML_TARFILE}
-wget -nv $AZUREML_TARFILEURI -P ${AZUREML_TMP}
-tar -zxf ${AZUREML_TMP}${AZUREML_TARFILE} -C ${AZUREML_INSTALLDIR}
-
-## Starting SparkBatch as service
-echo "Starting azureml spark-batch as service"
-cat >/etc/systemd/system/multi-user.target.wants/azureml.service <<EOL
-[[Unit]
-Description=azureml service
-
-[Service]
-Type=simple
-User=root
-Environment=YARN_CONF_DIR=
-Environment=SPARK_HOME=/dsvm/tools/spark/current
-Environment=PYSPARK_PYTHON=/anaconda/bin/python
-Environment=LIVY_URL=http://localhost:8998
-Restart=always
-RestartSec=5
-ExecStart=${AZUREML_INSTALLDIR}/azureml/bin/catalina.sh run
-ExecStopPost=rm -rf $CATALINA_HOME/temp/*
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-systemctl daemon-reload
-systemctl stop azureml.service
-systemctl start azureml.service
-
 ## Install AML CLI
-echo "Installing azureml cli"
+echo "Installing Azure Machine Learning Command Line Tools"
 
 ## First upgrade pip and setuptools to avoid running into this issue:
 ## https://github.com/GoogleCloudPlatform/google-cloud-python/issues/2990
