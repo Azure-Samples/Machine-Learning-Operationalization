8760c551f2bf6c70ec2640fa029c0b8eec884404
RaymondL <raymondlaghaeian@users.noreply.github.com>
2017-09-19 09:49:48 -0700

    Added key

--- samples/python/code/newsgroup/pythonclient.py
+++ samples/python/code/newsgroup/pythonclient.py
@@ -5,7 +5,8 @@ data = "{\"doc_text\": \"OpenGL on the GPU is fast\"}"
 body = str.encode(json.dumps(data))
 
 url = 'http://127.0.0.1:32769/score' 
-headers = {'Content-Type':'application/json'}
+api_key = 'your service key'  
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)} 
 
 resp = requests.post(url, data, headers=headers)
 print(resp.text)
